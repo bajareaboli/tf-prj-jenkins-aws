4b1d230d5c9bbd4cdd94c93d2131843930dd256d
Aboli <bajareaboli5@gmail.com>
2021-10-14 09:46:21 +1100

    key removed

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@
 provider "aws" {
   region = var.region
   #shared_credentials_file = "~/.aws/credentials"
-  access_key = "AKIA3BALKX5BEJANLAQL"
-  secret_key = "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"
+  access_key = "add key"
+  secret_key = "add key"
 
   # profile                 = "development"
 }
